2291da5348234a9f9e5aba8db1ca95b67d9d5bd7
的場郁美 <>
2020-06-11 13:46:05 +0900

    fadeIn完了

--- js/main.js
+++ js/main.js
@@ -4,22 +4,80 @@ $(function(){
   // 矢印がクリックされたら、スムーススクロール
   $('.arrow-up').click(() => {
       $('body, html').animate({ scrollTop: 0,}, 600);
-      
   })
+
   
   // 画面がスクロールされたら
   $(window).scroll(function () {
       
       // 画面のスクロールされた量を取得
       let scrollTop = $(document).scrollTop();
-      // console.log(scrollTop);
-      
-      // スクロールの量が500以上なら表示、それ以外は非表示
+      // スクロールの量が300以上なら表示、それ以外は非表示
       if (scrollTop >= 300) {
           $('.arrow-up').fadeIn();
       } else {
           $('.arrow-up').fadeOut();
       }
+
+
+      let scene2Top = $('.scene2').offset().top;
+      if (scrollTop >= scene2Top -190) {
+          $('.scene2-text').fadeIn();
+      } else {
+          $('.scene2-text').fadeOut();
+      }
+
+      let scene4Top = $('.scene4').offset().top;
+      if (scrollTop >= scene4Top -190) {
+          $('.scene4-text').fadeIn();
+      } else {
+          $('.scene4-text').fadeOut();
+      }
+
+      let scene10Top = $('.scene10').offset().top;
+      if (scrollTop >= scene10Top -190) {
+          $('.scene10-text').fadeIn();
+      } else {
+          $('.scene10-text').fadeOut();
+      }
+
+      let scene11Top = $('.scene11').offset().top;
+      if (scrollTop >= scene11Top -500) {
+          $('.scene11-text').fadeIn();
+      } else {
+          $('.scene11-text').fadeOut();
+      }
+
+
+
+      // 画面表示時アニメーション
+      let scene5Top = $('.scene5').offset().top;
+      if (scrollTop >= scene5Top -550) {
+          $('.scene5').animate({ left: 0}, 1000);
+      }
+
+      let scene6Top = $('.scene6').offset().top;
+      if (scrollTop >= scene6Top -550) {
+          $('.scene6').animate({ left: 0}, 1000);
+      }
+
+      let scene7Top = $('.scene7').offset().top;
+      if (scrollTop >= scene7Top -550) {
+          $('.scene7').animate({ left: 0}, 1000);
+      }
+
+      let scene8Top = $('.scene8').offset().top;
+      if (scrollTop >= scene8Top -550) {
+          $('.scene8').animate({ left: 0}, 1000);
+      }
+
+      let scene9Top = $('.scene9').offset().top;
+      if (scrollTop >= scene9Top -550) {
+          $('.scene9').animate({ left: 0}, 1000);
+      }
+
+    
+
       
   });
 })
